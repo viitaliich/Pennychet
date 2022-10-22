08a3fa4f3cff5f114c5f5457165d82ef7a5a4a9c
Vitalii Klimov <vitklim11@gmail.com>
2022-10-22 11:21:21 +0300

    Basic UI

--- Pennychet/app/src/main/java/com/example/pennychet/MainActivity.java
+++ Pennychet/app/src/main/java/com/example/pennychet/MainActivity.java
@@ -1,56 +1,102 @@
 package com.example.pennychet;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
-import android.view.HapticFeedbackConstants;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.navigation.NavController;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-//import com.google.android.material.appbar.MaterialToolbar;
-import androidx.appcompat.widget.Toolbar;
-
-import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Make sure to use the FloatingActionButton for all the FABs
+    // Floating Action Button (FAB)
     FloatingActionButton mAddFab, mAddAlarmFab, mAddPersonFab;
-
-    // These are taken to make visible and invisible along with FABs
     TextView addAlarmActionText, addPersonActionText;
-
-    // to check whether sub FAB buttons are visible or not.
     Boolean isAllFabsVisible;
 
-    private Toolbar toolbar;
+    // Drawer Layout
     private DrawerLayout drawerLayout;
-    private NavigationView navigationView;
-
 
     private static final String TAG = "MainActivity";
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: [STARTED]");
 
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        // ActionBar actionbar = getSupportActionBar();
+        // actionbar.setDisplayHomeAsUpEnabled(true);
+        // actionbar.setDisplayShowHomeEnabled(true);
+        // actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        // NavigationView navigationView = findViewById(R.id.nav_view);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        // FAB
+        mAddFab = findViewById(R.id.add_fab);
+        mAddAlarmFab = findViewById(R.id.add_alarm_fab);
+        mAddPersonFab = findViewById(R.id.add_person_fab);
+
+        addAlarmActionText = findViewById(R.id.add_alarm_action_text);
+        addPersonActionText = findViewById(R.id.add_person_action_text);
+
+        mAddAlarmFab.setVisibility(View.GONE);
+        mAddPersonFab.setVisibility(View.GONE);
+        addAlarmActionText.setVisibility(View.GONE);
+        addPersonActionText.setVisibility(View.GONE);
+        isAllFabsVisible = false;
+
+        mAddFab.setOnClickListener(view -> {
+            if (!isAllFabsVisible) {
+                mAddAlarmFab.show();
+                mAddPersonFab.show();
+                addAlarmActionText.setVisibility(View.VISIBLE);
+                addPersonActionText.setVisibility(View.VISIBLE);
+                isAllFabsVisible = true;
+            } else {
+                mAddAlarmFab.hide();
+                mAddPersonFab.hide();
+                addAlarmActionText.setVisibility(View.GONE);
+                addPersonActionText.setVisibility(View.GONE);
+                isAllFabsVisible = false;
+            }
+        });
+
+        mAddPersonFab.setOnClickListener(
+                view -> Toast.makeText(MainActivity.this, "Person Added", Toast.LENGTH_SHORT
+                ).show());
+
+        mAddAlarmFab.setOnClickListener(
+                view -> Toast.makeText(MainActivity.this, "Alarm Added", Toast.LENGTH_SHORT
+                ).show());
+    }
+
+    // Toolbar
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.top_app_bar_menu, menu);
         return true;
     }
@@ -62,101 +108,12 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.search_toolbar_btn) {
             return true;
         }
+        // TODO
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        Log.d(TAG, "onCreate: [STARTED]");
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        toolbar = (Toolbar) findViewById(R.id.toolBar);
-        setSupportActionBar(toolbar);
-
-        ActionBar actionbar = getSupportActionBar();
-        actionbar.setDisplayHomeAsUpEnabled(true);
-        actionbar.setDisplayShowHomeEnabled(true);
-        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
-
-        drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.nav_view);
-
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                drawerLayout.openDrawer(GravityCompat.START);
-            }
-        });
-
-        // Register all the FABs with their IDs This FAB button is the Parent
-        mAddFab = findViewById(R.id.add_fab);
-
-
-        // FAB button
-        mAddAlarmFab = findViewById(R.id.add_alarm_fab);
-        mAddPersonFab = findViewById(R.id.add_person_fab);
-
-        // Also register the action name text, of all the FABs.
-        addAlarmActionText = findViewById(R.id.add_alarm_action_text);
-        addPersonActionText = findViewById(R.id.add_person_action_text);
-
-        // Now set all the FABs and all the action name texts as GONE
-        mAddAlarmFab.setVisibility(View.GONE);
-        mAddPersonFab.setVisibility(View.GONE);
-        addAlarmActionText.setVisibility(View.GONE);
-        addPersonActionText.setVisibility(View.GONE);
-
-        // make the boolean variable as false, as all the
-        // action name texts and all the sub FABs are invisible
-        isAllFabsVisible = false;
-
-        // We will make all the FABs and action name texts
-        // visible only when Parent FAB button is clicked So
-        // we have to handle the Parent FAB button first, by
-        // using setOnClickListener you can see below
-        mAddFab.setOnClickListener(view -> {
-            if (!isAllFabsVisible) {
-                // when isAllFabsVisible becomes true make all
-                // the action name texts and FABs VISIBLE
-                mAddAlarmFab.show();
-                mAddPersonFab.show();
-                addAlarmActionText.setVisibility(View.VISIBLE);
-                addPersonActionText.setVisibility(View.VISIBLE);
-
-                // make the boolean variable true as we
-                // have set the sub FABs visibility to GONE
-                isAllFabsVisible = true;
-            } else {
-                // when isAllFabsVisible becomes true make
-                // all the action name texts and FABs GONE.
-                mAddAlarmFab.hide();
-                mAddPersonFab.hide();
-                addAlarmActionText.setVisibility(View.GONE);
-                addPersonActionText.setVisibility(View.GONE);
-
-                // make the boolean variable false as we
-                // have set the sub FABs visibility to GONE
-                isAllFabsVisible = false;
-            }
-        });
-        // below is the sample action to handle add person FAB. Here it shows simple Toast msg.
-        // The Toast will be shown only when they are visible and only when user clicks on them
-        mAddPersonFab.setOnClickListener(
-                view -> Toast.makeText(MainActivity.this, "Person Added", Toast.LENGTH_SHORT
-                ).show());
-
-        // below is the sample action to handle add alarm FAB. Here it shows simple Toast msg
-        // The Toast will be shown only when they are visible and only when user clicks on them
-        mAddAlarmFab.setOnClickListener(
-                view -> Toast.makeText(MainActivity.this, "Alarm Added", Toast.LENGTH_SHORT
-                ).show());
     }
 
 }
@@ -226,4 +183,3 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate: [FINISHED]");
 
          */
-
